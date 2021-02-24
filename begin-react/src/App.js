ab712d3eedd80f8e250ca7176d3024338a19c935
manggong <jihwank1995@gmail.com>
2021-02-24 21:53:20 +0900

    add style

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,10 +3,20 @@ import Hello from './hello'
 import './App.css';
 
 function App() {
+  const name = 'manggong Kim'
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '24',
+    padding: '1rem'
+  }
   return (
     <>
       <div className="App">
         <Hello/>
+      </div>
+      <div style = {style}>
+        {name}
       </div>
     </>
   );
