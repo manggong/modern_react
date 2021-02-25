769ae96bc95828de347ba02d4f9e049d41975566
manggong <jihwank1995@gmail.com>
2021-02-25 22:13:25 +0900

    if rendering

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -13,7 +13,7 @@ function App() {
   }
   return (
     <Wrapper>
-        <Hello style = {style}/>
+        <Hello style = {style} isSpecial={true}/>
     </Wrapper>
   );
 }
--- begin-react/src/hello.js
+++ begin-react/src/hello.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-function Hello({style, name}) {
-    return <div style = {style}>안녕하십니까? 오랜만에 리액트로 뵙네여~ {name} </div>
+function Hello({style, name, isSpecial}) {
+    return (
+        <div style = {style}>
+            {isSpecial ? <b>*</b> : null}
+            안녕하십니까? 오랜만에 리액트로 뵙네여~ {name} 
+        </div>
+        )
 }
 
 Hello.defaultProps = {
