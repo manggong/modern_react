ca08f50b6b4972206585dceb81b5aa2d5eeb397e
manggong <jihwank1995@gmail.com>
2021-03-06 10:19:24 +0900

    add useRef

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import Counter from './counter'
-import InputSample from './InputSample'
-import './App.css';
+import React, { useRef } from 'react';
+import UserList from './UserList';
 
 function App() {
-  const name = 'manggong Kim'
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '24',
-    padding: '1rem'
-  }
-  return (
-    <>
-      <Counter/>
-      <InputSample/>
-    </>
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;
