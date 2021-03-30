4acd8b53120452cd95ca097dd9fc3196dedaab94
manggong <jihwank1995@gmail.com>
2021-03-30 20:00:32 +0900

    Modify todos

--- mashup-todolist/src/components/TodoCreate.js
+++ mashup-todolist/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import styled, { css } from 'styled-components'
-import { MdAdd } from 'react-icons/md'
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -15,7 +16,6 @@ const CircleButton = styled.button`
   cursor: pointer;
   width: 80px;
   height: 80px;
-  display: block;
   align-items: center;
   justify-content: center;
   font-size: 60px;
@@ -27,9 +27,7 @@ const CircleButton = styled.button`
   border-radius: 50%;
   border: none;
   outline: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+
 
   transition: 0.125s all ease-in;
   ${props =>
@@ -44,14 +42,14 @@ const CircleButton = styled.button`
       }
       transform: translate(-50%, 50%) rotate(45deg);
     `}
-`
+`;
 
 const InsertFormPositioner = styled.div`
   width: 100%;
   bottom: 0;
   left: 0;
   position: absolute;
-`
+`;
 
 const InsertForm = styled.form`
   background: #f8f9fa;
@@ -63,7 +61,7 @@ const InsertForm = styled.form`
   border-bottom-left-radius: 16px;
   border-bottom-right-radius: 16px;
   border-top: 1px solid #e9ecef;
-`
+`;
 
 const Input = styled.input`
   padding: 12px;
@@ -73,19 +71,43 @@ const Input = styled.input`
   outline: none;
   font-size: 18px;
   box-sizing: border-box;
-`
+`;
 
 function TodoCreate() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
 
-  const onToggle = () => setOpen(!open)
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
+  const onToggle = () => setOpen(!open);
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault(); // 새로고침 방지
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -93,7 +115,7 @@ function TodoCreate() {
         <MdAdd />
       </CircleButton>
     </>
-  )
+  );
 }
 
-export default TodoCreate
+export default React.memo(TodoCreate);
--- mashup-todolist/src/components/TodoItem.js
+++ mashup-todolist/src/components/TodoItem.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import { MdDone, MdDelete } from 'react-icons/md'
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { MdDone, MdDelete } from 'react-icons/md';
+import { useTodoDispatch } from '../TodoContext';
 
 const Remove = styled.div`
   display: flex;
@@ -9,24 +10,23 @@ const Remove = styled.div`
   color: #dee2e6;
   font-size: 24px;
   cursor: pointer;
+  opacity: 0;
   &:hover {
     color: #ff6b6b;
   }
-  display: none;
-`
+`;
 
 const TodoItemBlock = styled.div`
   display: flex;
   align-items: center;
   padding-top: 12px;
   padding-bottom: 12px;
-
   &:hover {
     ${Remove} {
-      display: initial;
+      opacity: 1;
     }
   }
-`
+`;
 
 const CheckCircle = styled.div`
   width: 32px;
@@ -39,13 +39,13 @@ const CheckCircle = styled.div`
   justify-content: center;
   margin-right: 20px;
   cursor: pointer;
-  ${props => 
-    props.done && 
+  ${props =>
+    props.done &&
     css`
       border: 1px solid #38d9a9;
       color: #38d9a9;
     `}
-`
+`;
 
 const Text = styled.div`
   flex: 1;
@@ -54,22 +54,25 @@ const Text = styled.div`
   ${props =>
     props.done &&
     css`
-      color: #ced4da
+      color: #ced4da;
     `}
-`
+`;
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({ type: 'TOGGLE', id });
+  const onRemove = () => dispatch({ type: 'REMOVE', id });
   return (
     <TodoItemBlock>
-      <CheckCircle done={done}>{done && <MdDone/>}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
-        <MdDelete/>
+      <Remove onClick={onRemove}>
+        <MdDelete />
       </Remove>
     </TodoItemBlock>
-  )
+  );
 }
 
-
-export default TodoItem
-
+export default React.memo(TodoItem);
